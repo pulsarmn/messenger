26ebfec2c7d79d406f06872401814f15808cd009
pulsarmn <pulsarmn@yandex.ru>
2026-07-03 19:43:11 +0300

    feat: configure Argon2PasswordEncoder in the SecurityConfiguration

---
+++ src/main/java/org/pulsar/messenger/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package org.pulsar.messenger.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    PasswordEncoder defaultPasswordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+}
