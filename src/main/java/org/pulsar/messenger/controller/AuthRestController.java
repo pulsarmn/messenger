35f8d0e74a74984ac493e836ec27e16d41ed10a8
pulsarmn <152626412+pulsarmn@users.noreply.github.com>
2026-07-05 09:29:26 +0300

    refactor(auth): distribute the registration logic into different classes
    * refactor: extract registration business logic to the AuthService
    
    * feat: add exception handling for registration logic
    
    * docs: add spring note about @Transactional annotation

--- src/main/java/org/pulsar/messenger/controller/AuthRestController.java
+++ src/main/java/org/pulsar/messenger/controller/AuthRestController.java
@@ -3,12 +3,8 @@ package org.pulsar.messenger.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.pulsar.messenger.dto.RegistrationRequest;
-import org.pulsar.messenger.entity.User;
-import org.pulsar.messenger.exception.PasswordsMismatchException;
-import org.pulsar.messenger.repository.UserRepository;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.pulsar.messenger.service.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,38 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthRestController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@Validated @RequestBody RegistrationRequest registrationRequest) {
-        String password = registrationRequest.password();
-        String passwordConfirmation = registrationRequest.passwordConfirmation();
-
-        if (passwordsMatch(password, passwordConfirmation)) {
-            User user = mapToUser(registrationRequest);
-            String encodedPassword = passwordEncoder.encode(password);
-            user.setPasswordHash(encodedPassword);
-
-            try {
-                userRepository.save(user);
-            } catch (DataIntegrityViolationException e) {
-                throw new RuntimeException("Dolbaeb");
-            }
-        } else {
-            throw new PasswordsMismatchException();
-        }
+        authService.register(registrationRequest);
         return ResponseEntity.ok().build();
-    }
-
-    private User mapToUser(RegistrationRequest registrationRequest) {
-        return User.builder()
-                .username(registrationRequest.username())
-                .displayName(registrationRequest.username())
-                .build();
-    }
-
-    private boolean passwordsMatch(String sourcePassword, String passwordConfirmation) {
-        return sourcePassword != null && sourcePassword.equals(passwordConfirmation);
     }
 }
