4c618c061c4f6e6e1dac7ae3e1c1228ab3bd13ef
pulsarmn <pulsarmn@yandex.ru>
2026-07-07 17:15:05 +0300

    feat: implement token pair generating logic

---
+++ src/main/java/org/pulsar/messenger/config/ApplicationConfiguration.java
@@ -0,0 +1,17 @@
+package org.pulsar.messenger.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    Clock defaultClock() {
+        return Clock.systemUTC();
+    }
+}
---
+++ src/main/java/org/pulsar/messenger/service/AccessTokenGenerator.java
@@ -0,0 +1,44 @@
+package org.pulsar.messenger.service;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.interfaces.ECPrivateKey;
+import java.util.Map;
+
+
+@Component
+@RequiredArgsConstructor
+public class AccessTokenGenerator {
+
+    private final ECPrivateKey accessTokenPrivateKey;
+
+    public String generate(Map<String, Object> payload) {
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.ES384).build();
+        JWTClaimsSet claims = convertToClaims(payload);
+
+        try {
+            JWSSigner jwsSigner = new ECDSASigner(accessTokenPrivateKey, Curve.P_384);
+            SignedJWT token = new SignedJWT(header, claims);
+            token.sign(jwsSigner);
+
+            return token.serialize();
+        } catch (JOSEException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private JWTClaimsSet convertToClaims(Map<String, Object> payload) {
+        JWTClaimsSet.Builder claimsBuilder = new JWTClaimsSet.Builder();
+        payload.forEach(claimsBuilder::claim);
+        return claimsBuilder.build();
+    }
+}
