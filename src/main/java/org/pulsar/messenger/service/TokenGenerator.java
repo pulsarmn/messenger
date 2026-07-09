51f5773eb5aeea240f2f1de4bd155327ff05c23e
pulsarmn <pulsarmn@yandex.ru>
2026-07-09 09:46:25 +0300

    test: add tests for TokenGenerator class

---
+++ src/main/java/org/pulsar/messenger/service/TokenGenerator.java
@@ -0,0 +1,22 @@
+package org.pulsar.messenger.service;
+
+
+import org.springframework.stereotype.Component;
+import java.security.SecureRandom;
+
+
+@Component
+public class TokenGenerator {
+
+    private final SecureRandom secureRandom = new SecureRandom();
+
+    public byte[] generate(int tokenLength) {
+        if (tokenLength <= 0) {
+            return new byte[0];
+        }
+
+        byte[] tokenBytes = new byte[tokenLength];
+        secureRandom.nextBytes(tokenBytes);
+        return tokenBytes;
+    }
+}
