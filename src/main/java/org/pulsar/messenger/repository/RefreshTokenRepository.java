e88246655c673dbf2ae5fba89ae85155f268eeae
pulsarmn <pulsarmn@yandex.ru>
2026-07-09 14:36:51 +0300

    feat: implement an /api/auth/refresh endpoint

--- src/main/java/org/pulsar/messenger/repository/RefreshTokenRepository.java
+++ src/main/java/org/pulsar/messenger/repository/RefreshTokenRepository.java
@@ -1,10 +1,15 @@
 package org.pulsar.messenger.repository;
 
 import org.pulsar.messenger.entity.RefreshToken;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
 }
