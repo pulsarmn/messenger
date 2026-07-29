5fd957a5a6137d8d79b4cced903b39279362027c
pulsar <pulsarmn@yandex.ru>
2026-07-29 11:45:39 +0300

    feat(auth): implement the registration logic in the app

---
+++ src/main/java/ru/pulsarmn/messenger/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package ru.pulsarmn.messenger.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.pulsarmn.messenger.dto.RegistrationRequest;
+import ru.pulsarmn.messenger.entity.User;
+
+
+@Component
+public class UserMapper {
+
+    public User mapToEntity(RegistrationRequest request, String passwordHash) {
+        return User.builder()
+                .username(request.username())
+                .passwordHash(passwordHash)
+                .displayName("Default name") // TODO: extract this logic
+                .build();
+    }
+}
---
+++ src/main/java/ru/pulsarmn/messenger/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ru.pulsarmn.messenger.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.pulsarmn.messenger.entity.User;
+
+import java.util.UUID;
+
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByUsername(String username);
+}
