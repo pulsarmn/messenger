1ef8570cccf5f576f5fd9a2e8fbcb179f244e6dd
pulsar <pulsarmn@yandex.ru>
2026-08-02 18:30:39 +0300

    feat(auth): implemnt access token refreshing endpoint
    Implement a POST /api/v1/auth/refresh endpoint

--- src/main/java/ru/pulsarmn/messenger/repository/RefreshTokenRepository.java
+++ src/main/java/ru/pulsarmn/messenger/repository/RefreshTokenRepository.java
@@ -1,10 +1,15 @@
 package ru.pulsarmn.messenger.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.pulsarmn.messenger.entity.RefreshToken;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    @EntityGraph(attributePaths = "user")
+    Optional<RefreshToken> findByTokenHash(String refreshTokenHash);
 }
--- src/main/java/ru/pulsarmn/messenger/service/AuthService.java
+++ src/main/java/ru/pulsarmn/messenger/service/AuthService.java
@@ -3,6 +3,8 @@ package ru.pulsarmn.messenger.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.pulsarmn.messenger.dto.request.RefreshTokenRequest;
+import ru.pulsarmn.messenger.entity.RefreshToken;
 import ru.pulsarmn.messenger.jwt.factory.TokenPairFactory;
 import ru.pulsarmn.messenger.dto.request.AuthenticationRequest;
 import ru.pulsarmn.messenger.dto.request.RegistrationRequest;
@@ -13,7 +15,13 @@ import ru.pulsarmn.messenger.exception.PasswordMismatchException;
 import ru.pulsarmn.messenger.exception.UserAlreadyExistsException;
 import ru.pulsarmn.messenger.exception.UserNotFoundException;
 import ru.pulsarmn.messenger.mapper.UserMapper;
+import ru.pulsarmn.messenger.repository.RefreshTokenRepository;
 import ru.pulsarmn.messenger.repository.UserRepository;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.HexFormat;
 
 
 @Service
@@ -23,12 +31,18 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenPairFactory tokenPairFactory;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final HashService hashService;
+    private final Clock clock;
 
-    public AuthService(UserMapper userMapper, UserRepository userRepository, PasswordEncoder passwordEncoder, TokenPairFactory tokenPairFactory) {
+    public AuthService(UserMapper userMapper, UserRepository userRepository, PasswordEncoder passwordEncoder, TokenPairFactory tokenPairFactory, RefreshTokenRepository refreshTokenRepository, HashService hashService, Clock clock) {
         this.userMapper = userMapper;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.tokenPairFactory = tokenPairFactory;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.hashService = hashService;
+        this.clock = clock;
     }
 
     @Transactional
@@ -77,5 +91,31 @@ public class AuthService {
         }
 
         return tokenPairFactory.createTokenPair(user);
+    }
+
+    @Transactional
+    public TokenPairResponse refresh(RefreshTokenRequest request) {
+        byte[] refreshTokenBytes = Base64.getUrlDecoder().decode(request.oldRefreshToken());
+        byte[] hashBytes = hashService.hash(refreshTokenBytes);
+        String refreshTokenHash = HexFormat.of().formatHex(hashBytes);
+        RefreshToken refreshToken = refreshTokenRepository.findByTokenHash(refreshTokenHash)
+                .orElseThrow(() -> new BadCredentialsException("Invalid refresh token"));
+        checkExpiration(refreshToken);
+
+        User user = refreshToken.getUser();
+        refreshTokenRepository.delete(refreshToken);
+        return tokenPairFactory.createTokenPair(user);
+    }
+
+    private void checkExpiration(RefreshToken refreshToken) {
+        if (isExpired(refreshToken)) {
+            refreshTokenRepository.delete(refreshToken);
+            throw new BadCredentialsException("Refresh token has expired");
+        }
+    }
+
+    private boolean isExpired(RefreshToken refreshToken) {
+        Instant currentTime = Instant.now(clock);
+        return refreshToken.getExpiresAt().isBefore(currentTime);
     }
 }
