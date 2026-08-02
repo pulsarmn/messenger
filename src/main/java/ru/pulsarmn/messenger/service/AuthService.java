1e99417e6d671c377496028f408b1b156ef07570
pulsar <pulsarmn@yandex.ru>
2026-08-02 18:50:55 +0300

    refactor(auth): extract refresh token logic to RefreshTokenService

--- src/main/java/ru/pulsarmn/messenger/service/AuthService.java
+++ src/main/java/ru/pulsarmn/messenger/service/AuthService.java
@@ -3,25 +3,19 @@ package ru.pulsarmn.messenger.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.pulsarmn.messenger.dto.request.RefreshTokenRequest;
-import ru.pulsarmn.messenger.entity.RefreshToken;
-import ru.pulsarmn.messenger.jwt.factory.TokenPairFactory;
 import ru.pulsarmn.messenger.dto.request.AuthenticationRequest;
+import ru.pulsarmn.messenger.dto.request.RefreshTokenRequest;
 import ru.pulsarmn.messenger.dto.request.RegistrationRequest;
 import ru.pulsarmn.messenger.dto.response.TokenPairResponse;
+import ru.pulsarmn.messenger.entity.RefreshToken;
 import ru.pulsarmn.messenger.entity.User;
 import ru.pulsarmn.messenger.exception.BadCredentialsException;
 import ru.pulsarmn.messenger.exception.PasswordMismatchException;
 import ru.pulsarmn.messenger.exception.UserAlreadyExistsException;
 import ru.pulsarmn.messenger.exception.UserNotFoundException;
+import ru.pulsarmn.messenger.jwt.factory.TokenPairFactory;
 import ru.pulsarmn.messenger.mapper.UserMapper;
-import ru.pulsarmn.messenger.repository.RefreshTokenRepository;
 import ru.pulsarmn.messenger.repository.UserRepository;
-
-import java.time.Clock;
-import java.time.Instant;
-import java.util.Base64;
-import java.util.HexFormat;
 
 
 @Service
@@ -31,18 +25,14 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenPairFactory tokenPairFactory;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final HashService hashService;
-    private final Clock clock;
+    private final RefreshTokenService refreshTokenService;
 
-    public AuthService(UserMapper userMapper, UserRepository userRepository, PasswordEncoder passwordEncoder, TokenPairFactory tokenPairFactory, RefreshTokenRepository refreshTokenRepository, HashService hashService, Clock clock) {
+    public AuthService(UserMapper userMapper, UserRepository userRepository, PasswordEncoder passwordEncoder, TokenPairFactory tokenPairFactory, RefreshTokenService refreshTokenService) {
         this.userMapper = userMapper;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.tokenPairFactory = tokenPairFactory;
-        this.refreshTokenRepository = refreshTokenRepository;
-        this.hashService = hashService;
-        this.clock = clock;
+        this.refreshTokenService = refreshTokenService;
     }
 
     @Transactional
@@ -95,27 +85,18 @@ public class AuthService {
 
     @Transactional
     public TokenPairResponse refresh(RefreshTokenRequest request) {
-        byte[] refreshTokenBytes = Base64.getUrlDecoder().decode(request.oldRefreshToken());
-        byte[] hashBytes = hashService.hash(refreshTokenBytes);
-        String refreshTokenHash = HexFormat.of().formatHex(hashBytes);
-        RefreshToken refreshToken = refreshTokenRepository.findByTokenHash(refreshTokenHash)
-                .orElseThrow(() -> new BadCredentialsException("Invalid refresh token"));
-        checkExpiration(refreshToken);
+        RefreshToken refreshToken = refreshTokenService.find(request.oldRefreshToken());
+        checkRefreshTokenExpiration(refreshToken);
 
         User user = refreshToken.getUser();
-        refreshTokenRepository.delete(refreshToken);
+        refreshTokenService.delete(refreshToken);
         return tokenPairFactory.createTokenPair(user);
     }
 
-    private void checkExpiration(RefreshToken refreshToken) {
-        if (isExpired(refreshToken)) {
-            refreshTokenRepository.delete(refreshToken);
+    private void checkRefreshTokenExpiration(RefreshToken refreshToken) {
+        if (refreshTokenService.isExpired(refreshToken)) {
+            refreshTokenService.delete(refreshToken);
             throw new BadCredentialsException("Refresh token has expired");
         }
-    }
-
-    private boolean isExpired(RefreshToken refreshToken) {
-        Instant currentTime = Instant.now(clock);
-        return refreshToken.getExpiresAt().isBefore(currentTime);
     }
 }
--- src/main/java/ru/pulsarmn/messenger/service/RefreshTokenService.java
+++ src/main/java/ru/pulsarmn/messenger/service/RefreshTokenService.java
@@ -3,20 +3,52 @@ package ru.pulsarmn.messenger.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.pulsarmn.messenger.entity.RefreshToken;
+import ru.pulsarmn.messenger.exception.BadCredentialsException;
 import ru.pulsarmn.messenger.repository.RefreshTokenRepository;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.HexFormat;
 
 
 @Service
 public class RefreshTokenService {
 
+    private final Clock clock;
+    private final HashService hashService;
     private final RefreshTokenRepository refreshTokenRepository;
 
-    public RefreshTokenService(RefreshTokenRepository refreshTokenRepository) {
+    public RefreshTokenService(Clock clock, HashService hashService, RefreshTokenRepository refreshTokenRepository) {
+        this.clock = clock;
+        this.hashService = hashService;
         this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    public RefreshToken find(String rawRefreshToken) {
+        String refreshTokenHash = convertToHash(rawRefreshToken);
+        return refreshTokenRepository.findByTokenHash(refreshTokenHash)
+                .orElseThrow(() -> new BadCredentialsException("Invalid refresh token"));
+    }
+
+    private String convertToHash(String rawRefreshToken) {
+        byte[] refreshTokenBytes = Base64.getUrlDecoder().decode(rawRefreshToken);
+        byte[] hashBytes = hashService.hash(refreshTokenBytes);
+        return HexFormat.of().formatHex(hashBytes);
+    }
+
+    public boolean isExpired(RefreshToken refreshToken) {
+        Instant currentTime = Instant.now(clock);
+        return refreshToken.getExpiresAt().isBefore(currentTime);
     }
 
     @Transactional
     public RefreshToken save(RefreshToken refreshToken) {
         return refreshTokenRepository.save(refreshToken);
+    }
+
+    @Transactional
+    public void delete(RefreshToken refreshToken) {
+        refreshTokenRepository.delete(refreshToken);
     }
 }
