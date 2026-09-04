f7adb7e22a360537e76e1635c06a3834c458eef7
pulsar <pulsarmn@yandex.ru>
2026-09-04 11:25:30 +0300

    feat(messages): configure the STOMP settings in the StompConfiguration class

---
+++ src/main/java/ru/pulsarmn/messenger/config/StompConfiguration.java
@@ -0,0 +1,26 @@
+package ru.pulsarmn.messenger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .setUserDestinationPrefix("/user")
+                .enableSimpleBroker("/topic");
+    }
+}
