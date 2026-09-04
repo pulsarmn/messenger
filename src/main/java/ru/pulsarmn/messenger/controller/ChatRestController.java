132d8179cf5aded4aea53c560681ad7a6e38d1c5
pulsar <pulsarmn@yandex.ru>
2026-09-04 11:53:58 +0300

    refactor(messages): delete the sendMessage() method from the ChatRestController

--- src/main/java/ru/pulsarmn/messenger/controller/ChatRestController.java
+++ src/main/java/ru/pulsarmn/messenger/controller/ChatRestController.java
@@ -3,28 +3,23 @@ package ru.pulsarmn.messenger.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.pulsarmn.messenger.dto.request.ChatCreationRequest;
-import ru.pulsarmn.messenger.dto.request.MessageCreationRequest;
 import ru.pulsarmn.messenger.dto.response.ChatResponse;
-import ru.pulsarmn.messenger.dto.response.MessageResponse;
 import ru.pulsarmn.messenger.security.UserPrincipal;
 import ru.pulsarmn.messenger.service.DirectChatService;
-import ru.pulsarmn.messenger.service.MessageService;
-
-import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/api/v1/chats")
 public class ChatRestController {
 
-    private final MessageService messageService;
     private final DirectChatService directChatService;
 
-    public ChatRestController(MessageService messageService, DirectChatService directChatService) {
-        this.messageService = messageService;
+    public ChatRestController(DirectChatService directChatService) {
         this.directChatService = directChatService;
     }
 
@@ -33,13 +28,5 @@ public class ChatRestController {
         ChatResponse response = directChatService.getOrCreateDirectChat(userPrincipal.getUserId(), request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
-    }
-
-    @PostMapping("/{chatId}/messages")
-    ResponseEntity<MessageResponse> sendMessage(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                                @PathVariable UUID chatId,
-                                                @Validated @RequestBody MessageCreationRequest request) {
-        MessageResponse response = messageService.createMessage(userPrincipal.getUserId(), chatId, request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
