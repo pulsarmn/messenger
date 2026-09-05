d55c60f4dc6cc2d48b588012e2310844dee6a71d
pulsar <pulsarmn@yandex.ru>
2026-09-05 11:46:50 +0300

    feat(messages): add the findAllByChatId() method to the ChatMemberRepository

--- src/main/java/ru/pulsarmn/messenger/repository/ChatMemberRepository.java
+++ src/main/java/ru/pulsarmn/messenger/repository/ChatMemberRepository.java
@@ -1,9 +1,15 @@
 package ru.pulsarmn.messenger.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.w3c.dom.stylesheets.LinkStyle;
 import ru.pulsarmn.messenger.entity.ChatMember;
 import ru.pulsarmn.messenger.entity.ChatMemberId;
 
+import java.util.List;
+import java.util.UUID;
+
 
 public interface ChatMemberRepository extends JpaRepository<ChatMember, ChatMemberId> {
+
+    List<ChatMember> findAllByChatId(UUID chatId);
 }
