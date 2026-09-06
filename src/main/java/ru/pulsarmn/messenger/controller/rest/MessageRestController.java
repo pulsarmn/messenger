85f137d1fe31a42cf99332a1409c277efee9bbec
pulsar <pulsarmn@yandex.ru>
2026-09-06 12:22:49 +0300

    feat(messages): add an endpoint /api/v1/messages/{id} to update messages

---
+++ src/main/java/ru/pulsarmn/messenger/controller/rest/MessageRestController.java
@@ -0,0 +1,44 @@
+package ru.pulsarmn.messenger.controller.rest;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.pulsarmn.messenger.dto.MessageCreationResult;
+import ru.pulsarmn.messenger.dto.MessageEvent;
+import ru.pulsarmn.messenger.dto.request.MessageUpdateRequest;
+import ru.pulsarmn.messenger.security.UserPrincipal;
+import ru.pulsarmn.messenger.service.MessageService;
+
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageRestController {
+
+    private final MessageService messageService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    private static final String MESSAGES_TOPIC = "/queue/messages";
+
+    public MessageRestController(MessageService messageService, SimpMessagingTemplate messagingTemplate) {
+        this.messageService = messageService;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @PatchMapping("/messages/{id}")
+    ResponseEntity<Void> updateMessage(@AuthenticationPrincipal UserPrincipal userPrincipal,
+                                       @PathVariable UUID id,
+                                       @Validated @RequestBody MessageUpdateRequest request) {
+        MessageCreationResult result = messageService.updateMessage(userPrincipal.getUserId(), id, request);
+        MessageEvent messageEvent = new MessageEvent(MessageEvent.EventType.MESSAGE_UPDATED, result.messageResponse());
+        for (String username : result.recipientUsernames()) {
+            messagingTemplate.convertAndSendToUser(username, MESSAGES_TOPIC, messageEvent);
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
