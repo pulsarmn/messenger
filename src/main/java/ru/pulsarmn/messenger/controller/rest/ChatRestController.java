2d9bd2e11e1249b7bf60ed23d9effe6f9a6a1062
pulsar <pulsarmn@yandex.ru>
2026-09-07 12:27:52 +0300

    feat(messages): extract the getMessages() method from ChatRestController to MessageRestController

--- src/main/java/ru/pulsarmn/messenger/controller/rest/ChatRestController.java
+++ src/main/java/ru/pulsarmn/messenger/controller/rest/ChatRestController.java
@@ -3,27 +3,23 @@ package ru.pulsarmn.messenger.controller.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.pulsarmn.messenger.dto.request.ChatCreationRequest;
 import ru.pulsarmn.messenger.dto.response.ChatResponse;
-import ru.pulsarmn.messenger.dto.response.CursorPageResponse;
-import ru.pulsarmn.messenger.dto.response.MessageResponse;
 import ru.pulsarmn.messenger.security.UserPrincipal;
 import ru.pulsarmn.messenger.service.DirectChatService;
-import ru.pulsarmn.messenger.service.MessageService;
-
-import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/api/v1/chats")
 public class ChatRestController {
 
-    private final MessageService messageService;
     private final DirectChatService directChatService;
 
-    public ChatRestController(MessageService messageService, DirectChatService directChatService) {
-        this.messageService = messageService;
+    public ChatRestController(DirectChatService directChatService) {
         this.directChatService = directChatService;
     }
 
@@ -31,15 +27,6 @@ public class ChatRestController {
     ResponseEntity<ChatResponse> createChat(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestBody ChatCreationRequest request) {
         ChatResponse response = directChatService.getOrCreateDirectChat(userPrincipal.getUserId(), request);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(response);
-    }
-
-    @GetMapping("/{chatId}/messages")
-    ResponseEntity<CursorPageResponse<MessageResponse>> getMessages(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                                                    @PathVariable UUID chatId,
-                                                                    @RequestParam(value = "cursor", required = false) UUID cursorId) {
-        CursorPageResponse<MessageResponse> response = messageService.getMessages(userPrincipal.getUserId(), chatId, cursorId);
-        return ResponseEntity.status(HttpStatus.OK)
                 .body(response);
     }
 }
