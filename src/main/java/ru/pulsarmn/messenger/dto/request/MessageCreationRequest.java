1d9cf138f444efc3e8a87e4b2dc37e45d5f406b0
pulsar <pulsarmn@yandex.ru>
2026-09-07 08:22:14 +0300

    feat(messages): add validation annotations to the MessageCreationRequest

--- src/main/java/ru/pulsarmn/messenger/dto/request/MessageCreationRequest.java
+++ src/main/java/ru/pulsarmn/messenger/dto/request/MessageCreationRequest.java
@@ -1,7 +1,18 @@
 package ru.pulsarmn.messenger.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import ru.pulsarmn.messenger.entity.MessageType;
 
 
-public record MessageCreationRequest(MessageType messageType, String text) {
+public record MessageCreationRequest(
+
+        @NotNull
+        MessageType messageType,
+
+        @NotBlank
+        @Size(max = 4096)
+        String text
+) {
 }
